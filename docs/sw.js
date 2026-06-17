4f1f3b819c817b7f6c1d465a3a0c2c22b6754edb
terroba-l <leo.rouxterroba@gmail.com>
2026-06-18 01:07:46 +0200

    v44: imprint automatique sur tout echec d'auth
    Une chaussure fraiche (post-reset) NAK START_AUTHENTICATION (pas de cle), donc
    l'imprint doit se declencher sur tout echec d'auth, pas seulement sur no-key-match.
    Key-gen envoye une seule fois (dans imprint). Verifie en simulation: chaussure
    fraiche -> imprint+auth OK (1 key-gen); chaussure perimee -> refus -> factory reset.

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nike-adapt-v43";
+const CACHE = "nike-adapt-v44";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
