759416b322ee320b4d9a4d022e710cecf2b80956
terroba-l <leo.rouxterroba@gmail.com>
2026-06-18 01:30:12 +0200

    v45: diagnostic de connexion ameliore (log name+message+stack)

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nike-adapt-v44";
+const CACHE = "nike-adapt-v45";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
