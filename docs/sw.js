32223604974746c316c49cdb8c1105e85795a81b
terroba-l <leo.rouxterroba@gmail.com>
2026-06-17 20:38:21 +0200

    v43: logs distants ntfy ON (consentement utilisateur) + bouton toggle
    - Miroir de log vers ntfy.sh/nikeadapt-earl-3f9k2x7q active par defaut
      (l'utilisateur a explicitement consenti). Bouton on/off dans BLE Settings.
    - Permet de lire les logs de connexion BLE a distance pendant le debug imprint.

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nike-adapt-v42";
+const CACHE = "nike-adapt-v43";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
