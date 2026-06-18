12cd15c6c7572170c79c6d216bce76d8901cc6dd
terroba-l <leo.rouxterroba@gmail.com>
2026-06-18 09:11:41 +0200

    v46: retry de gatt.connect (4x) - connexion instable juste apres reset

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nike-adapt-v45";
+const CACHE = "nike-adapt-v46";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
